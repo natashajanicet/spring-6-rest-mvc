9506caf815172c3239a471d43ca594eca89c382d
Natasha <natashajanicetambunan@gmail.com>
2023-03-06 17:42:36 +0700

    Use lombok annotation @Slf4j to do loggin instead of sysout and enable the debug logging in application.properties

--- src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
@@ -1,11 +1,21 @@
 package natashalearn.springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import natashalearn.springframework.spring6restmvc.model.Beer;
 import natashalearn.springframework.spring6restmvc.services.BeerService;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+        log.debug("Get Beer by Id - in controller");
+        return beerService.getBeerById(id);
+    }
 }
--- src/main/java/natashalearn/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/natashalearn/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -1,15 +1,21 @@
 package natashalearn.springframework.spring6restmvc.services;
 
+import lombok.extern.slf4j.Slf4j;
 import natashalearn.springframework.spring6restmvc.model.Beer;
 import natashalearn.springframework.spring6restmvc.model.BeerStyle;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+        log.debug("Get Beer Id - in service");
+
         return Beer.builder()
                 .id(id)
                 .version(1)
