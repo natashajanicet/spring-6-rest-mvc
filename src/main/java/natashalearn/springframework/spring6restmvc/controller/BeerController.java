f14beeae04e3c36f306327a171a1ac850118b39b
Natasha <natashajanicetambunan@gmail.com>
2023-03-07 14:45:26 +0700

    Create response header to return location of the saved beer

--- src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import natashalearn.springframework.spring6restmvc.model.Beer;
 import natashalearn.springframework.spring6restmvc.services.BeerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +23,10 @@ public class BeerController {
     public ResponseEntity handlePost(@RequestBody Beer beer) {
         Beer savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
