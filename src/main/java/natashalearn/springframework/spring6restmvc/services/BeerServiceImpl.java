08579e0c03a250778a0a3e74a9fe222ee9287fb7
Natasha <natashajanicetambunan@gmail.com>
2023-03-07 14:18:12 +0700

    Create http post method in beercontroller to handle adding new beer

--- src/main/java/natashalearn/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/natashalearn/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -79,4 +79,22 @@ public class BeerServiceImpl implements BeerService {
                 .updatedDate(LocalDateTime.now())
                 .build();
     }
+
+    @Override
+    public Beer saveNewBeer(Beer beer) {
+        Beer savedBeer = Beer.builder()
+                .id(UUID.randomUUID())
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .beerName(beer.getBeerName())
+                .beerStyle(beer.getBeerStyle())
+                .quantityOnHand(beer.getQuantityOnHand())
+                .upc(beer.getUpc())
+                .price(beer.getPrice())
+                .build();
+
+        beerMap.put(savedBeer.getId(), savedBeer);
+
+        return savedBeer;
+    }
 }
