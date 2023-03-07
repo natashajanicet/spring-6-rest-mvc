9cb7bc6dbafcc3ad8ef5b445c9648dc8147f4545
Natasha <natashajanicetambunan@gmail.com>
2023-03-07 12:21:59 +0700

    Create list beer api endpoint

--- src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
@@ -5,14 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import natashalearn.springframework.spring6restmvc.model.Beer;
 import natashalearn.springframework.spring6restmvc.services.BeerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
         log.debug("Get Beer by Id - in controller");
