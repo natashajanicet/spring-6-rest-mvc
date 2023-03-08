908700dbda6f65a99cb38874481bda2c78777f03
Natasha <natashajanicetambunan@gmail.com>
2023-03-08 17:10:42 +0700

    Add @ExceptionHandler in controller to handle certain type of exception throw by all methods in the class

--- src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/natashalearn/springframework/spring6restmvc/controller/BeerController.java
@@ -57,6 +57,11 @@ public class BeerController {
         return beerService.listBeers();
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
     @GetMapping(BEER_PATH_ID)
     public Beer getBeerById(@PathVariable UUID beerId) {
         log.debug("Get Beer by Id - in controller");
