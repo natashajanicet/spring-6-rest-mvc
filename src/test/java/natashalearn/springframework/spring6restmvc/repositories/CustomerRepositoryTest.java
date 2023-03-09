32fd1ae35772704742f1b95574bf6b80374313b3
Natasha <natashajanicetambunan@gmail.com>
2023-03-09 12:50:59 +0700

    Create test for repository, test save new data

---
+++ src/test/java/natashalearn/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,23 @@
+package natashalearn.springframework.spring6restmvc.repositories;
+
+import natashalearn.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer savedCustomer = customerRepository.save(Customer.builder()
+                .customerName("New Customer Name").build());
+
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getId()).isNotNull();
+    }
+}
