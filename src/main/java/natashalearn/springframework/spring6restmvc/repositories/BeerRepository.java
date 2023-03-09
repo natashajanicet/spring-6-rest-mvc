b12ac925c804f254778cc88ac9b1c032cc6c883a
Natasha <natashajanicetambunan@gmail.com>
2023-03-09 08:43:52 +0700

    Create repository class

---
+++ src/main/java/natashalearn/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package natashalearn.springframework.spring6restmvc.repositories;
+
+import natashalearn.springframework.spring6restmvc.entities.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
